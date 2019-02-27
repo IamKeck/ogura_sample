a1540a71f5127dcbb2259f82a2fd70cc44e9f320
Kent Kawanaka <meal.is.tharihyu@gmail.com>
2019-02-27 12:05:13 +0900

    ArticleParserのテストを追加

--- tests/MyTest.elm
+++ tests/MyTest.elm
@@ -2,7 +2,8 @@ module MyTest exposing (..)
 
 import Expect exposing (Expectation)
 import Test exposing (..)
-import Blog exposing (isoTimeToDate)
+import Blog exposing (isoTimeToDate, articleParser, Article)
+import Json.Decode as D
 
 
 suite : Test
@@ -13,5 +14,13 @@ suite =
                 \_ -> isoTimeToDate "1990-10-23T12:00:00+09:00" |> Expect.equal "1990/10/23"
             , test "ゾーンに依存する日付" <|
                 \_ -> isoTimeToDate "1990-10-23T00:00:00+09:00" |> Expect.equal "1990/10/23"
+            ]
+        , describe "articleParserのテスト"
+            [ test "正常" <|
+                \_ -> D.decodeString
+                        articleParser
+                        "{\"title\":\"タイトル\",\"url\":\"http://hoge.com\",\"date\":\"1990-10-23T12:00:00+09:00\"}"
+                      |> Expect.equal (Result.Ok <| Article "タイトル" "http://hoge.com" "1990/10/23")
+
             ]
         ]
